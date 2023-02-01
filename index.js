fa9fe6b7d01210dd9bae48ce3492878414f9e533
DivyaSharmaaa <123402018+DivyaSharmaaa@users.noreply.github.com>
2023-02-01 05:18:55 -0800

    Update index.js

--- index.js
+++ index.js
@@ -45,5 +45,5 @@ const PORT = process.env.PORT || 5000
 const DATABASE_URL = process.env.CONNECTION_URL
 
 mongoose.connect(DATABASE_URL, { useNewUrlParser:true, useUnifiedTopology: true})
- .then(() => app.listen(PORT, () => {console.log(`server running on port ${process.env.PORT}`)}))
+ .then(() => app.listen(PORT, () => {console.log(`server running on port ${PORT}`)}))
  .catch((err) => console.log(err.message))
