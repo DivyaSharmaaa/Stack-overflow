5be90bd96fc0496ade767bf87994d9bd04e7d751
DivyaSharmaaa <123402018+DivyaSharmaaa@users.noreply.github.com>
2023-01-31 22:16:54 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express  from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv'
-import require from 'path'
+
 
 
 import userRoutes from './routes/users.js'
