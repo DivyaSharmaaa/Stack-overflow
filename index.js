2a648073e16f975027ba88945b19b671d93691b1
DivyaSharmaaa <123402018+DivyaSharmaaa@users.noreply.github.com>
2023-01-31 23:37:59 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express  from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv'
-import { createRequire } from "module";
 import * as path from 'path'
 
 
