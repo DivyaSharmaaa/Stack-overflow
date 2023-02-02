f6f3773601c24891baf264d6efb843e0672e6a5a
DivyaSharmaaa <123402018+DivyaSharmaaa@users.noreply.github.com>
2023-02-01 21:36:32 -0800

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,15 @@ import dotenv from 'dotenv'
 import * as path from 'path'
 
 
+import userRoutes from './routes/users.js'
+import questionRoutes from './routes/Questions.js'
+import answerRoutes from './routes/Answers.js'
+
+
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-
-
-
-import userRoutes from './routes/users.js'
-import questionRoutes from './routes/Questions.js'
-import answerRoutes from './routes/Answers.js'
 
 
 const app = express();
